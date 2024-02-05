0f562ee4847c54dfd7cd72b50f8603ed8cd4d580
Ekimov Sergey <es281082@ukr.net>
2024-02-05 23:04:22 +0200

    1

---
+++ N1.py
@@ -0,0 +1,24 @@
+
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        #якщо елемент відсутній у словнику, додаю елемент
+        if n not in cache:
+            cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+
+    return fibonacci
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
+
+        
